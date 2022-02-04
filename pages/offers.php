5620435518c7d1cd5314124a907b03241eb58777
ducstream-code <73170287+ducstream-code@users.noreply.github.com>
2022-02-04 22:59:55 +0100

    new offer page

--- pages/offers.php
+++ pages/offers.php
@@ -1,12 +1,14 @@
 <?php
 include ('../includes/db.php');
 include ('../includes/check_session.php');
+include('../includes/message.php');
+include ("../includes/header.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <link rel="stylesheet" href="/css/offers.css">
+    <link rel="stylesheet" href="../css/offers.css">
     <link rel="icon" type="image/png" href=""/>
     <meta charset="utf-8">
     <meta name="viewport" content="width-device-width, initial-scale=1">
@@ -16,40 +18,86 @@ include ('../includes/check_session.php');
             integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
             crossorigin="anonymous"></script>
 
-
-
     <title>Nos offres</title>
 </head>
-
-<body>
 <?php
 include('../includes/message.php');
 include ("../includes/header.php");
 ?>
-
-<?php
-$sql ="SELECT id,price,title,description,image FROM offers";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo'<div class="offers_container">';
-foreach ($res as $key => $offer_data) {
 ?>
-    <div class="card" style="width: 18rem;">
-        <img src="<?=$offer_data['image'] ?>" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><?=$offer_data['title'] ?></h5>
-            <p class="card-text"><?=substr($offer_data['description'],0,100).'...'; ?></p>
-            <a href="/pages/product?id=<?=$offer_data['id'] ?>" class="btn btn-primary">Voir l'offre</a>
+<body>
+
+<div id="offers_bubble" class="offers_bubble_container">
+    <div class="offer_container">
+        <div class="offer_header" onclick="big_bubble()">
+            <h1>Offres</h1>
+            <h1><ion-icon name="chevron-down-outline"></ion-icon></h1>
         </div>
-        <div class="card-footer">
-            <small class="text-muted">Cout en point: <?=$offer_data['price'] ?></small>
+
+        <div class="offers">
+            <?php
+            $stmt = $db->prepare("SELECT briev_description, store_image FROM discounts LIMIT 6");
+            $stmt->execute();
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res as $key => $discount){
+            ?>
+            <div class="offer_card">
+                <div class="offer_image">
+                    <img src="..<?=$discount['store_image']?>>">
+                </div>
+                <div class="offer_footer">
+                    <?= $discount['briev_description']?>
+                </div>
+            </div>
+            <?php
+            }
+            ?>
+
         </div>
+
     </div>
+
+</div>
+<div id="shop_bubble" class="offers_bubble_container">
+    <div class="offer_container">
+        <div class="offer_header">
+            <h1>Boutique</h1>
+            <h1><ion-icon name="chevron-down-outline"></ion-icon></h1>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="shop_container"></div>
+
+<script>
+    function big_bubble() {
+        let bubble = document.getElementById('offers_bubble');
+        let bubble_inside = document.querySelector('.offer_container');
+        let offers = document.querySelector('.offers')
+        bubble.style.marginTop = '70px';
+        bubble_inside.style.marginTop = '70px';
+        bubble.style.height='300px';
+        bubble_inside.style.height='300px';
+        offers.style.display='flex';
+        document.querySelector('#offers_bubble .offer_header').setAttribute("onClick", "little_bubble()");
+    }
+
+    function little_bubble() {
+        let bubble = document.getElementById('offers_bubble');
+        let bubble_inside = document.querySelector('.offer_container');
+        let offers = document.querySelector('.offers')
+
+        bubble.style.marginTop = '200px';
+        bubble_inside.style.marginTop = '200px';
+        bubble.style.height='75px'
+        bubble_inside.style.height='75px'
+        offers.style.display='none';
+
+        document.querySelector('#offers_bubble .offer_header').setAttribute("onClick", "big_bubble();");
+    }
+</script>
 </body>
 </html>
-<?php
-}
-/*
- *
- */
+
