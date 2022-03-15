6f18b1e7f3a1ce8b9bbed99ea61d360d693fd3d6
Mathieu <mathdub78@gmail.com>
2022-03-15 10:16:51 +0100

    payment error and succes pages

---
+++ pages/order_error.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php include "../includes/head.php"?>
+    <title>Error!</title>
+  </head>
+  <body>
+
+
+
+<div class="center pt-5  mt-24">
+
+<p class="primary text-6xl text-center font-bold">Mince, une erreur est survenue</p>
+
+<div class="inner text-center pl-10 pr-10">
+
+<p class="secondary text-4xl text-gray-400">votre payment n'a pas pu aboutir suite à une erreur de payment</p>
+
+</div>
+
+<div class="flex flex justify-center pt-10">
+<img class="object-contain h-64 "src="../assets/images/animated/error.gif">
+</div>
+</p>
+
+
+</div>
+
+
+
+
+  </body>
+</html>
---
+++ pages/order_succes.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php include "../includes/head.php"?>
+    <title></title>
+  </head>
+  <body>
+
+
+
+<div class="center pt-5  mt-24">
+
+<p class="primary text-5xl text-center font-bold">Merci, votre commande a bien été effectuée !</p>
+
+<div class="inner text-center pl-10 pr-10">
+
+<p class="secondary text-3xl text-gray-400">merci pour votre confiance, votre commande sera traitée dans les plus brefs délais</p>
+
+</div>
+
+<div class="flex flex justify-center pt-10">
+<img class="object-contain h-64 "src="../assets/images/animated/done.gif">
+</div>
+</p>
+
+
+</div>
+
+
+
+
+  </body>
+</html>
